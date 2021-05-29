67c9fa4af0c3f85d073b5375d224ae3ac23d73f2
Tanmay Singal <tanmaysingal@icloud.com>
2021-05-29 13:59:16 +0530

    IIIFConfig file that extends IngestConfig

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IngestConfig.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IngestConfig.java
@@ -63,9 +63,6 @@ public class IngestConfig implements ExtractionContextProvider {
     /** Configuration for extraction-pipeline. Defaults to global configuration. */
     private final CacheConfig cacheConfig;
 
-    /** IIIF configuration used to fetch media files from remote servers. */
-    private IIIFConfig iiifConfig;
-
     /**
      * Constructor for {@link IngestConfig}. Used by Jackson for JSON deserialization.
      *
