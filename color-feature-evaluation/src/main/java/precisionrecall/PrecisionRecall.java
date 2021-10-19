ac879fa92936049357b7e9d25288715b52c42207
rahel.arnold <rahel.arnold@unibas.ch>
2021-10-19 12:58:27 +0200

    precision and recall computation adapted to query numbers and separate JSON config file for paths

--- color-feature-evaluation/src/main/java/precisionrecall/PrecisionRecall.java
+++ color-feature-evaluation/src/main/java/precisionrecall/PrecisionRecall.java
@@ -1,25 +1,39 @@
 package precisionrecall;
 
+import automatedQueries.ColorFeatureEvaluation;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class PrecisionRecall {
 
-  public static String path;
+  public static String path_results;
 
   private static int numberOfRelevantSegments;
 
   private static float x;
 
+  private static ArrayList<Entry> dict = new ArrayList<>();
+
 
   public static void main(String[] args) {
 
-    path = "results";
-    int numberOfQueries = 100;
+    ColorFeatureEvaluation colorFeatureEvaluation = new ColorFeatureEvaluation();
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      colorFeatureEvaluation = mapper.readValue(new File("ColorFeatureEvaluation.json"), ColorFeatureEvaluation.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-    numberOfRelevantSegments = 50;
+    path_results = "json-queries";
+    String path_dict = colorFeatureEvaluation.getPathDictionary() + "/" + colorFeatureEvaluation.getNameDictionary();
+    int numberOfQueries = 3;
+
+    numberOfRelevantSegments = 10;
 
     // not yet used
     int errorAcceptance = 1; // +/- 1 segments considered right --> looking for 3 segments
@@ -27,6 +41,14 @@ public class PrecisionRecall {
     x = 0;
     float y = numberOfQueries * numberOfRelevantSegments;
     float z = numberOfQueries * (2 + errorAcceptance + 1);
+
+    try {
+      dict = mapper.readValue(new File(path_dict),
+          new TypeReference<ArrayList<Entry>>() {
+          });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
     checkSegments();
     //System.out.println(Arrays.deepToString(res));
@@ -43,52 +65,79 @@ public class PrecisionRecall {
   }
 
   private static void checkSegments() {
-    File dir = new File(path);
+    File dir = new File(path_results);
     File[] directoryListing = dir.listFiles();
     if (directoryListing != null) {
       for (File child : directoryListing) {
-        // get name of relevant segment
-        if (child.getName().startsWith("sketch")) {
-          String nameTopSegment = "v" + child.getName().substring(6);
-          nameTopSegment = nameTopSegment.substring(0, nameTopSegment.length() - 5);
+        String count = child.getName().substring(6, child.getName().indexOf("."));
+        for (Entry entry : dict) {
+          if (entry.getNumber() == Integer.parseInt(count)){
+            // get name of relevant segment
+            if (entry.getName().startsWith("sketch")) {
+              String nameTopSegment = "v" + entry.getName().substring(6);
+              nameTopSegment = nameTopSegment.substring(0, nameTopSegment.length() - 4);
 
-          String nameLowerSegment;
-          int id = Integer.parseInt(
-              nameTopSegment.substring(nameTopSegment.lastIndexOf("_") + 1).trim());
-          int idm = id - 1;
-          nameLowerSegment = nameTopSegment.substring(0, nameTopSegment.lastIndexOf("_") + 1) + idm;
+              String nameLowerSegment;
+              int id = Integer.parseInt(
+                  nameTopSegment.substring(nameTopSegment.lastIndexOf("_") + 1).trim());
+              int idm = id - 1;
+              nameLowerSegment =
+                  nameTopSegment.substring(0, nameTopSegment.lastIndexOf("_") + 1) + idm;
 
-          int idp = id + 1;
-          String nameHigherSegment;
-          nameHigherSegment =
-              nameTopSegment.substring(0, nameTopSegment.lastIndexOf("_") + 1) + idp;
+              int idp = id + 1;
+              String nameHigherSegment;
+              nameHigherSegment =
+                  nameTopSegment.substring(0, nameTopSegment.lastIndexOf("_") + 1) + idp;
 
-          QueryInfo queryInfo = new QueryInfo();
-          try {
-            ObjectMapper mapper = new ObjectMapper();
-            queryInfo = mapper.readValue(new File(child.getAbsolutePath()), QueryInfo.class);
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-
-          int i = 0;
-          for (Segment s : queryInfo.segments) {
-            if (i < numberOfRelevantSegments) {
-              if (Objects.equals(s.segment_id, nameTopSegment)) {
-                x = x + 3;
-                break;
-              } else if (Objects.equals(s.segment_id, nameLowerSegment)) {
-                x = x + 1;
-              } else if (Objects.equals(s.segment_id, nameHigherSegment)) {
-                x = x + 1;
+              QueryInfo queryInfo = new QueryInfo();
+              try {
+                ObjectMapper mapper = new ObjectMapper();
+                queryInfo = mapper.readValue(new File(child.getAbsolutePath()), QueryInfo.class);
+              } catch (IOException e) {
+                e.printStackTrace();
               }
-              i++;
-            } else {
-              break;
+
+              int i = 0;
+              for (Segment s : queryInfo.segments) {
+                if (i < numberOfRelevantSegments) {
+                  if (Objects.equals(s.segment_id, nameTopSegment)) {
+                    x = x + 3;
+                    break;
+                  } else if (Objects.equals(s.segment_id, nameLowerSegment)) {
+                    x = x + 1;
+                  } else if (Objects.equals(s.segment_id, nameHigherSegment)) {
+                    x = x + 1;
+                  }
+                  i++;
+                } else {
+                  break;
+                }
+              }
             }
           }
         }
       }
+    }
+  }
+
+  static class Entry {
+
+    String name;
+    int number;
+
+    Entry(String name, int number) {
+      this.name = name;
+      this.number = number;
+    }
+
+    Entry(){}
+
+    public String getName() {
+      return name;
+    }
+
+    public int getNumber() {
+      return number;
     }
   }
 
