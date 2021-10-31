33973f812a8988ec851ed3f93f1db8dd0a88be14
rahel.arnold <rahel.arnold@unibas.ch>
2021-10-31 18:04:08 +0000

    corrected paths for extraction

--- extraction/createExtractionConfig.sh
+++ extraction/createExtractionConfig.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for i in $(seq 1 32); do
-  cp ../extract_to_json_vbs.json ../extraction_config/extract_to_json_vbs$i.json
+  cp extract_to_json_vbs_p1.json extraction_config/extract_to_json_vbs_p1_$i.json
   # input path
-  sed -i "s/\"path\": \"pathSource\"/\"path\": \"\/mnt\/hdd\/extractionV3C2\/video_split${i}\"/g" ../extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"path\": \"pathSource\"/\"path\": \"\/mnt\/hdd\/extractionV3C2\/video_split${i}\"/g" extraction_config/extract_to_json_vbs$i.json
   # path thumbnails
-  sed -i "s/\"destination\": \"pathThumbnails\/\"/\"destination\": \"\/mnt\/hdd\/thumbnails${i}\"/g" ../extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"destination\": \"pathThumbnails\/\"/\"destination\": \"\/mnt\/hdd\/thumbnails${i}\"/g" extraction_config/extract_to_json_vbs$i.json
   # path output
-  sed -i "s/\"host\": \"pathOutput\/\"/\"host\": \"\/mnt\/hdd\/output${i}\"/g" ../extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"host\": \"pathOutput\/\"/\"host\": \"\/mnt\/hdd\/output${i}\"/g" extraction_config/extract_to_json_vbs$i.json
 done
--- extraction/createTmux.sh
+++ extraction/createTmux.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # create session
 tmux new-session -d -s extraction
-for i in $(seq 1 3); do
+for i in $(seq 1 32); do
   # create windows
   tmux send-keys -t extraction "tmux new-window -n split${i} " ENTER
 done
 
-for i in $(seq 1 3); do
+for i in $(seq 1 32); do
   # start cineast
-  tmux send-keys -t extraction "tmux send-keys -t split${i} 'java -jar ../cineast-api/build/libs/cineast-api-3.4.0-SNAPSHOT-all.jar ../cineast_config/cineast${i}.json' ENTER" ENTER
+  tmux send-keys -t extraction "tmux send-keys -t split${i} 'java -jar cineast-api/build/libs/cineast-api-3.4.0-SNAPSHOT-all.jar cineast_config/cineast${i}.json' ENTER" ENTER
 done
 
 # wait to be sure that cineast started
 sleep 20s
 
-for i in $(seq 1 3); do
+for i in $(seq 1 32); do
   # start extraction
-  tmux send-keys -t extraction "tmux send-keys -t split${i} 'extract --extraction ../extraction_config/extract_to_json_job_${i}.json' ENTER" ENTER
+  tmux send-keys -t extraction "tmux send-keys -t split${i} 'extract --extraction extraction_config/extract_to_json_vbs_p1_${i}.json' ENTER" ENTER
 done
 
