b4e6e9ae4c34eb4ce8debff96f9e92edd592df4c
rahel.arnold <rahel.arnold@unibas.ch>
2021-10-31 11:57:34 +0100

    changed paths of extraction files

--- extraction/changePropetheus.sh
+++ extraction/changePropetheus.sh
@@ -3,5 +3,5 @@ a=24
 for i in $(seq 1 32); do
   j=$((a+i))
   k=$(printf "%02d" $i)
-  sed -i "${j}i\\ \\ \\ \\ \\ \\ - targets: ['localhost:48${k}']" docs/linux/prometheus.yml
+  sed -i "${j}i\\ \\ \\ \\ \\ \\ - targets: ['localhost:48${k}']" ../docs/linux/prometheus.yml
 done
