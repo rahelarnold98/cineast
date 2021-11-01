4f2466475c268ee17be8e2f06130b2ef39aebd5f
rahel.arnold <rahel.arnold@unibas.ch>
2021-11-01 07:40:58 +0000

    corrected extraction config files

--- extraction/createExtractionConfig.sh
+++ extraction/createExtractionConfig.sh
@@ -3,9 +3,9 @@
 for i in $(seq 1 32); do
   cp extract_to_json_vbs_p1.json extraction_config/extract_to_json_vbs_p1_$i.json
   # input path
-  sed -i "s/\"path\": \"pathSource\"/\"path\": \"\/mnt\/hdd\/extractionV3C2\/video_split${i}\"/g" extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"path\": \"pathSource\"/\"path\": \"\/mnt\/hdd\/extractionV3C2\/video_split${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
   # path thumbnails
-  sed -i "s/\"destination\": \"pathThumbnails\/\"/\"destination\": \"\/mnt\/hdd\/thumbnails${i}\"/g" extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"destination\": \"pathThumbnails\/\"/\"destination\": \"\/mnt\/hdd\/thumbnails${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
   # path output
-  sed -i "s/\"host\": \"pathOutput\/\"/\"host\": \"\/mnt\/hdd\/output${i}\"/g" extraction_config/extract_to_json_vbs$i.json
+  sed -i "s/\"host\": \"pathOutput\/\"/\"host\": \"\/mnt\/hdd\/output${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
 done
