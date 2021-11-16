844a9175bc55668bf4f59cb5d64009d09e9ce1ce
rahel.arnold <rahel.arnold@unibas.ch>
2021-11-16 13:32:36 +0100

    superpixel features for AverageColorGrid8Reduced15

---
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/features/AverageColorGrid8Reduced15SuperpixelFh04.java
@@ -0,0 +1,73 @@
+package org.vitrivr.cineast.core.features;
+
+import boofcv.abst.segmentation.ImageSuperpixels;
+import boofcv.factory.segmentation.ConfigFh04;
+import boofcv.factory.segmentation.FactoryImageSegmentation;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.ImageBase;
+import boofcv.struct.image.ImageType;
+import boofcv.struct.image.Planar;
+import java.awt.image.BufferedImage;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.vitrivr.cineast.core.config.CacheConfig;
+import org.vitrivr.cineast.core.config.QueryConfig;
+import org.vitrivr.cineast.core.config.ReadableQueryConfig;
+import org.vitrivr.cineast.core.data.FloatVector;
+import org.vitrivr.cineast.core.data.Pair;
+import org.vitrivr.cineast.core.data.ReadableFloatVector;
+import org.vitrivr.cineast.core.data.raw.CachedDataFactory;
+import org.vitrivr.cineast.core.data.raw.images.MultiImage;
+import org.vitrivr.cineast.core.data.score.ScoreElement;
+import org.vitrivr.cineast.core.data.segments.SegmentContainer;
+import org.vitrivr.cineast.core.util.ColorReductionUtil;
+
+public class AverageColorGrid8Reduced15SuperpixelFh04 extends AverageColorGrid8 {
+
+  private static final Logger LOGGER = LogManager.getLogger();
+
+  private final CacheConfig cacheConfig = new CacheConfig("AUTOMATIC", ".");
+  private final CachedDataFactory factory = new CachedDataFactory(cacheConfig);
+
+  public AverageColorGrid8Reduced15SuperpixelFh04() {
+    super("features_AverageColorGrid8Reduced15SuperpixelFh04", 12595f / 4f);
+  }
+
+  @Override
+  public void processSegment(SegmentContainer shot) {
+    if (shot.getAvgImg() == MultiImage.EMPTY_MULTIIMAGE) {
+      return;
+    }
+    if (!phandler.idExists(shot.getId())) {
+      BufferedImage superpixel = applySuperpixel(shot);
+
+      MultiImage multiImage = factory.newMultiImage(superpixel);
+      MultiImage avgimg = ColorReductionUtil.quantize15(multiImage);
+      persist(shot.getId(), partition(avgimg).first);
+    }
+  }
+
+  @Override
+  public List<ScoreElement> getSimilar(SegmentContainer sc, ReadableQueryConfig qc) {
+    BufferedImage superpixel = applySuperpixel(sc);
+
+    MultiImage multiImage = factory.newMultiImage(superpixel);
+    Pair<FloatVector, float[]> p = partition(ColorReductionUtil.quantize15(multiImage));
+    return getSimilar(ReadableFloatVector.toArray(p.first), new QueryConfig(qc).setDistanceWeights(p.second));
+  }
+
+  private BufferedImage applySuperpixel(SegmentContainer segmentContainer) {
+
+    BufferedImage image = segmentContainer.getAvgImg().getBufferedImage();
+    image = ConvertBufferedImage.stripAlphaChannel(image);
+    ImageType<Planar<GrayF32>> imageType = ImageType.pl(3, GrayF32.class);
+    ImageSuperpixels alg = FactoryImageSegmentation.fh04(new ConfigFh04(100, 30), imageType);
+    ImageBase color = imageType.createImage(image.getWidth(), image.getHeight());
+    ConvertBufferedImage.convertFrom(image, color, true);
+    BufferedImage superpixel = Superpixel.performSegmentation(alg, color);
+    return superpixel;
+  }
+
+}
