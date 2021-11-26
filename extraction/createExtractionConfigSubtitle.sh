e942fb1eaf5504e63507652a851556b1ea4c3a9c
rahel.arnold <rahel.arnold@unibas.ch>
2021-11-26 15:15:12 +0100

    extraction files for SubtitleFullTextSearch

---
+++ extraction/createExtractionConfigSubtitle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in $(seq 1 32); do
+  cp extract_SubtitleFulltextSearch.json extraction_config/extract_SubtitleFulltextSearch_$i.json
+  # input path
+  sed -i "s/\"path\": \"pathSource\"/\"path\": \"\/mnt\/hdd\/extractionV3C2\/video_split${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
+  # path thumbnails
+  sed -i "s/\"destination\": \"pathThumbnails\/\"/\"destination\": \"\/mnt\/hdd\/thumbnails${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
+  # path output
+  sed -i "s/\"host\": \"pathOutput\/\"/\"host\": \"\/mnt\/hdd\/outputSubtitle${i}\"/g" extraction_config/extract_to_json_vbs_p1_$i.json
+done
