e74068ac8176cce13a3bb9275c40befb1ddd6dbc
rahel.arnold <rahel.arnold@unibas.ch>
2021-11-27 14:42:14 +0100

    file to start tmux sessions

---
+++ extraction/createTmuxP2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# create session
+tmux new-session -d -s extraction
+for i in $(seq 1 4); do
+  # create windows
+  tmux send-keys -t extraction "tmux new-window -n split${i} " ENTER
+done
+
+for i in $(seq 1 4); do
+  # start cineast
+  tmux send-keys -t extraction "tmux send-keys -t split${i} 'java -jar cineast-api/build/libs/cineast-api-3.4.0-SNAPSHOT-all.jar cineast_config/cineast${i}.json' ENTER" ENTER
+done
+
+# wait to be sure that cineast started
+sleep 20s
+
+
+for i in $(seq 1 4); do
+  # start extraction
+  tmux send-keys -t extraction:split${i} "extract --extraction extraction_config/extract_OCR_${i}.json" C-m
+done
+
